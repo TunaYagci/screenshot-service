f1ce69bbcefcd2a6e042475e9d6900dbeee490ed
Tuna Yagci <tuna.yagci@atarlabs.io>
2019-12-23 12:37:42 +0300

    WIP dockerize server and postgres

---
+++ deployment/postgres/schema.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE screenshot;
+CREATE USER screenshot with password 'screenshot123';
+GRANT ALL PRIVILEGES on DATABASE screenshot to screenshot;
--- event-registry/src/main/java/com/tunayagci/screenshot/eventregistry/EventRegistryApplication.java
+++ event-registry/src/main/java/com/tunayagci/screenshot/eventregistry/EventRegistryApplication.java
@@ -1,13 +1,10 @@
 package com.tunayagci.screenshot.eventregistry;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class EventRegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EventRegistryApplication.class, args);
     }
+
 
 }
